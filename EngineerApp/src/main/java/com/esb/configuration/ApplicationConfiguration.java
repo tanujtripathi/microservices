d9c600cc28f07b12f628892e3df9637fa93c59bc
tanujtripathi <51440867+tanujtripathi@users.noreply.github.com>
2019-06-07 12:29:25 +0530

    Add files via upload

--- EngineerApp/src/main/java/com/esb/configuration/ApplicationConfiguration.java
+++ EngineerApp/src/main/java/com/esb/configuration/ApplicationConfiguration.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-@Configuration
-@PropertySources({
-        @PropertySource("classpath:constants.properties"),
-        @PropertySource("classpath:application.properties")
-})
+//@Configuration
+//@PropertySources({
+//        @PropertySource("classpath:constants.properties"),
+//        @PropertySource("classpath:application.properties")
+//})
 public class ApplicationConfiguration {
 
     ApplicationConfiguration() {
